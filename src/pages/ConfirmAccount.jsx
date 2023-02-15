7d4a4f16030ebb51b8b01b877d81752a2218a95f
Victor Seleimend <vsennaselei@gmail.com>
2023-02-15 17:53:17 -0300

    confirmando a conta

--- src/pages/ConfirmAccount.jsx
+++ src/pages/ConfirmAccount.jsx
@@ -1,5 +1,12 @@
 const ConfirmAccount = () => {
-  return <div>ConfirmAccount</div>
-}
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Confirme sua conta e comece a criar seus{' '}
+        <span className="text-slate-700">projetos</span>
+      </h1>
+    </>
+  );
+};
 
 export default ConfirmAccount;
