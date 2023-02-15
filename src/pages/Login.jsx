790d91c2be4f1f66585960462d6fab12d6cda649
Victor Seleimend <vsennaselei@gmail.com>
2023-02-15 16:52:11 -0300

    começando a criação do form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,18 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Iniciar sessão e administrar seus{' '}
+        <span className="text-slate-700">projetos</span>
+      </h1>
+      <form>
+        <div>
+          <label>Email</label>
+          <input type={'email'} placeholder="Digite seu e-mail" className="" />
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default Login;
