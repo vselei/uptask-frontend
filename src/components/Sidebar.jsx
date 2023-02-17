6c3a0a4070df307ee3407e5169300c368cc04c2c
Victor Seleimend <vsennaselei@gmail.com>
2023-02-17 13:55:13 -0300

    exibindo nome do usuario

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 
+import useAuth from '../hooks/useAuth';
+
 const Sidebar = () => {
+  const { auth } = useAuth();
+
   return (
     <aside className="md:w-80 lg:w-96 px-5 py-10">
-      <p className="text-xl font-bold">Olá, Victor</p>
+      <p className="text-xl font-bold">Olá, {auth.name}</p>
       <Link
         to={'create-project'}
         className="bg-sky-600 w-full uppercase font-bold p-3 text-white block mt-5 text-center rounded-lg"
