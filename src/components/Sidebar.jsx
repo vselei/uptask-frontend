3725afa055868d518533959f5cfaabfe9bcc8f96
Victor Seleimend <vsennaselei@gmail.com>
2023-02-17 13:48:56 -0300

    fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <aside className="md:w-80 lg:w-96 px-5 py-10">
       <p className="text-xl font-bold">Olá, Victor</p>
-      <Link to={'create-project'} className="bg-sky-600 w-full uppercase font-bold p-3 text-white">Novo Projeto</Link>
+      <Link
+        to={'create-project'}
+        className="bg-sky-600 w-full uppercase font-bold p-3 text-white block mt-5 text-center rounded-lg"
+      >
+        Novo Projeto
+      </Link>
     </aside>
   );
 };
