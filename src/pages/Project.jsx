5aac4e5d52dfb1af693f6b545e29eaff4ac042a2
Victor Seleimend <vsennaselei@gmail.com>
2023-02-20 09:50:22 -0300

    botão de adição de tarefas

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -34,9 +34,37 @@ const Project = () => {
   const data = useLoaderData();
 
   return (
-    <div className="flex justify-between">
-      <h1 className="font-black text-4xl">{data?.project?.name}</h1>
-      <div className="flex items-center gap-2 text-gray-400 hover:text-black">
+    <>
+      <div className="flex justify-between">
+        <h1 className="font-black text-4xl">{data?.project?.name}</h1>
+        <div className="flex items-center gap-2 text-gray-400 hover:text-black">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+            />
+          </svg>
+          <Link
+            className="uppercase font-bold"
+            to={`/projects/edit/${data?.params.id}`}
+          >
+            Editar
+          </Link>
+        </div>
+      </div>
+
+      <button
+        type="button"
+        className="text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center mt-5 flex gap-2 items-center justify-center"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -48,17 +76,12 @@ const Project = () => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-        <Link
-          className="uppercase font-bold"
-          to={`/projects/edit/${data?.params.id}`}
-        >
-          Editar
-        </Link>
-      </div>
-    </div>
+        Adicionar Tarefa
+      </button>
+    </>
   );
 };
 
