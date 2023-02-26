1cd113e47fa214593f958cede0bd58cc5c68eb02
Victor Seleimend <vsennaselei@gmail.com>
2023-02-26 11:08:19 -0300

    exbindo colaboradores

--- src/components/Collab.jsx
+++ src/components/Collab.jsx
@@ -1,5 +1,22 @@
-const Collab = ({collaborator}) =>{
-  return <p>hello</p>
-}
+const Collab = ({ collaborator }) => {
+  const { name, email } = collaborator;
+
+  return (
+    <div className="border-b p-5 flex justify-between items-center">
+      <div>
+        <p>{name}</p>
+        <p className="text-gray-700 text-sm">{email}</p>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="bg-red-600 px-4  py-3 text-white uppercase font-bold text-sm rounded-lg"
+        >
+          Deletar
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Collab;
