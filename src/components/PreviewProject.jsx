4ce832eb9e10f58f4e8e41768b0c6614faa7a22f
Victor Seleimend <vsennaselei@gmail.com>
2023-02-27 12:36:32 -0300

    exibindo se o projeto é seu ou se é colaborador

--- src/components/PreviewProject.jsx
+++ src/components/PreviewProject.jsx
@@ -1,11 +1,26 @@
 import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const PreviewProject = ({ project }) => {
-  const { name, _id, client } = project;
+  const { auth } = useAuth();
+
+  const { name, _id, client, creator } = project;
 
   return (
-    <div className="border-b p-5 flex">
-      <p className="flex-1">{name} <span className='text-sm text-gray-500 uppercase'>{client}</span></p>
+    <div className="border-b p-5 flex justify-between">
+      <div className="flex items-center gap-2">
+        <p className="flex-1">
+          {name}{' '}
+          <span className="text-sm text-gray-500 uppercase">{client}</span>
+        </p>
+
+        {auth._id !== creator && (
+          <p className="p-1 text-xs rounded-lg text-white bg-green-500 font-bold uppercase">
+            Colaborador
+          </p>
+        )}
+      </div>
+
       <Link
         to={`${_id}`}
         className="text-gray-600 hover:text-gray-800 uppercase text-sm font-bold"
